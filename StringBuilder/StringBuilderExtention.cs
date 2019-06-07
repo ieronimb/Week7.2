41a32e02422106c34fe1382c4bba5bf4c8b175e3
Birnat Ieronim <Ieronimb@outlook.com>
2019-06-07 16:41:22 +0300

    added new exercise

---
+++ StringBuilder/StringBuilderExtention.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilderExtension
+{
+    public static class SubstringExt
+    {
+       
+        public static StringBuilder Substring(this StringBuilder sb, int Index, int Length)
+        {
+            StringBuilder Current = new StringBuilder();
+
+            for (int i = Index; i < i + Length; i++)
+            {
+                Current.Append(sb[i]);
+            }
+            return Current;
+        }
+    }
+}
---
+++ StringBuilder/StringBuilderSubstring.cs
@@ -0,0 +1,26 @@
+﻿using StringBuilderExtension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilderSubstring
+{
+    class StringBuilderSubstring
+    {
+        /*Problem 1. StringBuilder.Substring
+        Implement an extension method Substring(int index, int length) for the class StringBuilder that returns new StringBuilder and has 
+        the same functionality as Substring in the class String.*/
+
+        static void Main(string[] args)
+        {
+            StringBuilder myString = new StringBuilder();
+            myString.Append("Testing method extention");
+            Console.WriteLine(myString.Substring(3, 9));            
+
+            Console.ReadKey();
+        }
+
+    }
+}
